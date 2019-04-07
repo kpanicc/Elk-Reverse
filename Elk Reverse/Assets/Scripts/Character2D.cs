525dc3cab084e50cd562b39e571d78081b72264b
Unknown <jcordobarom@gmail.com>
2019-04-07 23:36:16 +0200

    Jumping added

--- Elk Reverse/Assets/Scripts/Character2D.cs
+++ Elk Reverse/Assets/Scripts/Character2D.cs
@@ -4,29 +4,46 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour {
 
-    private float maxSpeed = 8f;
+    [SerializeField] private float m_maxSpeed = 8f;
+    [SerializeField] private float m_JumpForce = 400f;
+    [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
     private Animator animator;
     private Rigidbody2D rb2d;
+    private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+    const float k_GroundedRadius = .2f;
+
+    private bool m_Grounded;
     private bool facingLeft = true;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+        m_GroundCheck = transform.Find("GroundCheck");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
+        }
+        animator.SetBool("Grounded", m_Grounded);
+    }
 
     public void Move(float direction)
     {
         animator.SetFloat("SpeedX", Mathf.Abs(direction));
         //Output the current Animation name and length to the screen
-        rb2d.velocity = new Vector2(direction * maxSpeed, rb2d.velocity.y);
+        rb2d.velocity = new Vector2(direction * m_maxSpeed, rb2d.velocity.y);
 
         if (facingLeft && direction > 0)
         {
@@ -38,9 +55,15 @@ public class Character2D : MonoBehaviour {
         }
     }
 
+    public void Jump()
+    {
+        if (m_Grounded)
+            rb2d.AddForce(new Vector2(0, m_JumpForce));
+    }
+
     private void OnGUI()
     {
-
+        //print("SpeedX: " + rb2d.velocity);
     }
 
     public void Flip()
--- Elk Reverse/Assets/Scripts/PlayerController.cs
+++ Elk Reverse/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Character2D character;
+    private bool m_Jump = false;
 
 	// Use this for initialization
 	void Awake ()
     {
         character = GetComponent<Character2D>();	
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = Input.GetButtonDown("Jump");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
         float direction = Input.GetAxis("Horizontal");
         character.Move(direction);
+        if (m_Jump)
+        {
+            character.Jump();
+            m_Jump = false;
+        }
     }
 }
